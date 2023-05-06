7bb1ac2258255472797e1a1ceab6402b8be74e76
jenny-hub90 <jumbledjenny19@gmail.com>
2023-05-06 18:43:00 +0545

    added feedback andanimation on deleting and adding of feedbacks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
@@ -9,6 +10,12 @@ import Button from "./shared/Button";
 
 const App = () => {
    const [feedback, setFeedback] = useState(FeedbackData)
+   
+   const addFeedback = (newFeedback) =>{
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback,...feedback])
+   }
+
    const deleteFeedback = (id) =>{
     if(window.confirm('Are you sure you want to delete?'))
     {
@@ -19,7 +26,7 @@ return(
     <>
     <Header/>
     <div className='container'>
-        <FeedbackForm />
+        <FeedbackForm handleAdd ={addFeedback}/>
         <Button/>
         <FeedbackStats feedback={feedback}/>
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>   
